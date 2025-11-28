f765dd1264d5c769ec98a13a31f9be19c60bc681
Martin <martingalindo3202@gmail.com>
2025-11-28 02:56:48 -0600

    Update routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import Productos from './Pages/Productos';
-import Experiencia from './Pages/Experiencia';
-import Ubicacion from './Pages/Ubicacion';
-import Contacto from './Pages/Contacto';
-import Unete from './Pages/Unete';
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Productos from './Pages/Productos'
+import Experiencia from './Pages/Experiencia'
+import Ubicacion from './Pages/Ubicacion'
+import Contacto from './Pages/Contacto'
+import Unete from './Pages/Unete'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        ;<Route path="/" element={<Home />} />
-        <Route path="/productos" element={<Productos />} />
-        <Route path="/experiencia" element={<Experiencia />} />
-        <Route path="/ubicacion" element={<Ubicacion />} />
-        <Route path="/contacto" element={<Contacto />} />
-        <Route path="/unete" element={<Unete />} />
-      </Routes>
-    </Router>
-  );
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/productos" element={<Productos />} />
+      <Route path="/experiencia" element={<Experiencia />} />
+      <Route path="/ubicacion" element={<Ubicacion />} />
+      <Route path="/contacto" element={<Contacto />} />
+      <Route path="/unete" element={<Unete />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
